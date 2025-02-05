20f7b44f22d2bc5a480e7cf17ec5d4a653836aaf
khushijain06 <codingwithkhushi@gmail.com>
2025-02-05 14:19:26 +0530

    second commit

---
+++ Password generator/src/App.jsx
@@ -0,0 +1,13 @@
+import  Button from "./components/button"
+
+function App() {
+  return (
+    <>
+     <div className="h-screen  bg-gradient-to-r from-blue-400 to-blue-700">
+      <Button />
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ Password generator/src/components/button.jsx
@@ -0,0 +1,14 @@
+
+// eslint-disable-next-line react/prop-types
+function Button({height = '20',width ='40',content ='default button'}){
+  return(
+    <>
+        <button className={`h-${height} w-${width} text-amber-50 bg-gradient-to-r from-indigo-500 to-indigo-700  rounded-2xl px-6 py-3 my-1.5 shadow-lg transition-transform transform hover:-translate-y-1  cursor-pointer`}>
+    {content}
+</button>
+
+    </>
+  )
+}
+
+export default Button;
