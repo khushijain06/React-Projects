caf178c96106a952daa52081ff0f7affece6d4dc
khushijain06 <codingwithkhushi@gmail.com>
2025-03-08 15:57:58 +0530

    apihub

--- Smartify/src/components/WeatherCard/Mausam.jsx
+++ Smartify/src/components/WeatherCard/Mausam.jsx
@@ -1,30 +1,37 @@
 import { useEffect, useState } from "react";
-import { useSelector ,useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { getLocation } from "../../store/latlongSlice";
+import { FaWind } from "react-icons/fa";
+import { TbTemperatureSun } from "react-icons/tb";
+import { WiHumidity } from "react-icons/wi";
 
-
-
-function WeatherData({styles}) {
-  const dispatch=useDispatch()
+function WeatherData({ styles }) {
+  const dispatch = useDispatch();
   const { lat, lon } = useSelector((state) => state.latlong);
   const [error, seterror] = useState(null);
   const [load, setload] = useState(true);
   //const [aqi,setaqi] = useState(0)
   const [location, setlocation] = useState("Delhi");
   const [temp, settemp] = useState(0);
+  const [feelslike, setfeelslike] = useState(0);
   const [weather, setweather] = useState("clear");
   const [humidity, sethumidity] = useState(0);
   const [windspeed, setwindspeed] = useState(0);
+  const [winddeg, setwinddeg] = useState(0);
+  const [pressure, setpressure] = useState(0);
+  const [mintemp, setmintemp] = useState(0);
+  const [maxtemp, setmaxtemp] = useState(0);
+
   const apikey = "540c20df6282dc94ea35ff28a407f44b";
 
-   useEffect(() => {
-         dispatch(getLocation())
-      }, [dispatch]);
+  useEffect(() => {
+    dispatch(getLocation());
+  }, [dispatch]);
 
   useEffect(() => {
     const fetchdata = async () => {
       try {
-        console.log(`${lat} && ${lon}`)
+        console.log(`${lat} && ${lon}`);
         const response = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`
         );
@@ -36,6 +43,11 @@ function WeatherData({styles}) {
         sethumidity(data.main.humidity);
         setweather(data.weather[0].description);
         setwindspeed(data.wind.speed);
+        setmintemp(data.main.temp_min);
+        setmaxtemp(data.main.temp_max);
+        setwinddeg(data.wind.deg);
+        setfeelslike(data.main.feels_like);
+        setpressure(data.main.pressure);
         setload(false);
       } catch (error) {
         seterror(error.message);
@@ -44,22 +56,42 @@ function WeatherData({styles}) {
     };
     fetchdata();
   }, [lat, lon, apikey]);
-  return ( 
-                <div className={styles}>
-                  {load ? (
-                    <p>Loading...</p>
-                  ) : error ? (
-                    <p>Error: {error}</p>
-                  ) : (
-                    <div>
-                   <p className="pb-1">Weather in {location} </p>
-                      <p className="pb-1">Temperature: {(temp - 273.15).toFixed(2)}°C</p>
-                      <p className="pb-1">Weather: {weather}</p>
-                      <p className="pb-1">Humidity: {humidity}%</p>
-                      <p >Wind Speed: {windspeed} m/s</p>
-                    </div>
-                  )}
-                </div>
-  )
+  return (
+    <div className="flex flex-wrap gap-4 items-center">
+      <div className={`${styles} flex-grow`}>
+        {load ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: {error}</p>
+        ) : (
+          <div>
+            <TbTemperatureSun />
+            <h1 className="text-5xl mb-2">{(temp - 273.15).toFixed(2)}°C</h1>
+            <p className="text-2xl mb-2"> {weather}</p>
+            <p> Feels like : {(feelslike - 273.15).toFixed(2)}°C</p>
+            <p> Min Temp: {mintemp} </p>
+            <p>Max Temp: {maxtemp}</p>
+          </div>
+        )}
+      </div>
+      <div className="space-y-4">
+      <div className={`${styles} flex-grow`}>
+        <FaWind />
+        <p className="m-2">
+          Wind Speed: {windspeed}
+          <br />
+          Wind Degree: {winddeg}°
+        </p>
+      </div>
+      <div className={`${styles} flex-grow`}>
+        <WiHumidity />
+        <p>Humidity : {humidity}%
+          <br/>
+        Air Pressure : {pressure} hPa
+        </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default WeatherData;
--- Smartify/src/components/WeatherCard/Weather.jsx
+++ Smartify/src/components/WeatherCard/Weather.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Mausam from './Mausam'
-import Aqi from './Aqi'
+import React from "react";
+import Mausam from "./Mausam";
+import Aqi from "./Aqi";
 
-function Weather({parentstyles , childstyles}) {
+function Weather({ parentstyles, childstyles }) {
   return (
-    <div className={parentstyles}>
-      <Mausam styles={childstyles}/>
-      <Aqi styles={childstyles}/>
-    </div>
-  )
+    <>
+      <div className={parentstyles}>
+        <div className="items-start flex">
+        <span className="relative flex size-3">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
+          <span className="relative inline-flex size-3 rounded-full bg-red-500"></span>
+        </span>
+        </div>
+        <Mausam styles={childstyles} />
+        <Aqi styles={childstyles} />
+      </div>
+    </>
+  );
 }
 
-export default Weather
+export default Weather;
